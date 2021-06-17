6de1140d53f319133c7855fd71990fe02e6e8a62
ashley-aguayo <ashley.aguayo@outlook.com>
2021-06-16 22:31:22 -0700

    updaye

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask,render_template, redirect
-from flask_pymongo import pymongo
+from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
 
 #set up mongo connection
-app.config["MONGO_URI"]= "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 #route to render index.html 
 @app.route("/")
 def home():
+    mars_data_dict = mongo.db.collection.find_one()
+
+    return render_template("index.html",mars_data=mars_data_dict)
+
+@app.route("/scrape")
+def scrape():
+    mars_data = scrape_mars.scrape_info()
     
+    mongo.db.collection.update({},mars_data, upsert=True)
+
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -18,3 +18,28 @@ def scrape_info():
     html = browser.html
     soup = bs(html, "html.parser")
     
+    #get the news_title and news_p
+    news_title = soup.find('div', class_='content_title')[0].text
+    news_p=soup.find_all('div', class_='article_teaser_body')[0].text
+
+    #get the featured image
+    jpl_url ='https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    featured_image_url='https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/image/featured/mars1.jpg'
+
+    #get the hemisphere dict
+    hemisphere_image_urls = [
+    {"title": "Ceberus Hemisphere Enhanced", 'img_url':'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'},
+    {"title": "Schiaparelli Hemisphere Enhanced", 'img_url':'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'},
+    {"title": "Syrtis Major Hemisphere Enhanced", 'img_url':'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'},
+    {"title": "Valles Marineris Hemisphere Enhanced", 'img_url':'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}
+    ]
+
+    #create dict for all variables
+    mars_data_dict = {
+        "news_titles": news_title,
+        "news_paragraph": news_p,
+        "feat_image": featured_image_url,
+        "hemisphere_images": hemisphere_image_urls
+    }
+    browser.quit()
+    return mars_data_dict
